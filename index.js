d5d9a2f0656ce46c328661be371c2ba667e8df23
Rene <rwalker9935@gmail.com>
2018-08-13 15:27:13 -0400

    push2

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+$(window).on("load", function(){
+/*  $.ajax({
+    url: "https://newsapi.org/v2/top-headlines?country=us&apiKey=f7c291e55d624f92a436de14705d5899&category=health",
+    type: "GET",
+    data: {}
+  }).done(function(data){
+    console.log(data)
+  })*/
+  getArticles("health");
+})
+
+function getArticles(type= "") {
+  var apiUrl= "https://newsapi.org/v2/top-headlines?country=us&apiKey=f7c291e55d624f92a436de14705d5899";
+  var url= apiUrl+"&category="+type;
+  $.ajax({
+    url: url,
+    type: "GET",
+    data: {}
+  }).done(function(data){
+    displayArticles(data.articles)
+  })
+}
+
+function displayArticles(data) {
+  //add first image to page//
+  document.getElementById("art1title").innerHTML=data[0].title;
+  document.getElementById("art1description").innerHTML=data[0].description;
+/*  document.getElementById("art1").innerHTML=data[0].title;var articleUrl= data[0].url;*/
+  document.getElementById("art1img").innerHTML= `<img src="${data[0].urlToImage}">`
+  //data[0].title;var imageUrl= data[0].urlToImage;//
+}
+
+/*function buildArticleHTML(article) {
+  var title= article.title;
+  var description= article.description;
+  var articleUrl= article.url;
+  var imageUrl= article.urlToImage;
+}*/
+
 function dropdown() {
   document.getElementById("dropdown").classList.toggle("dropdownShow")
 }
